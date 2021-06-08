1511f7dea3893e2412b33a510c9a0c0b24da9051
Bruno De Masi <bruno.demasi@terras.agr.br>
2021-06-08 01:16:05 -0300

    refreshing token

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { setCookie, parseCookies } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -10,3 +10,34 @@ export const api = axios.create({
   }
 })
 
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies();
+
+      const { 'nextauth.refreshToken': refreshToken } = cookies = parseCookies();
+
+      api.post('/refresh', {
+        refreshToken
+      }).then(response => {
+        const { token } = response.data;
+
+        setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days,
+          path: '/'
+        });
+
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days,
+          path: '/'
+        });
+
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+      })
+    } else {
+      // logout user
+    }
+  }
+})
