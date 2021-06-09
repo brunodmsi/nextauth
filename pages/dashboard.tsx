c3e054d77dbb46e550fe0fbfd12fb11051ab4e48
Bruno De Masi <brunodemasi1@gmail.com>
2021-06-08 21:11:56 -0300

    validating auth through server

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,21 @@
 import { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
-import { api } from '../services/api';
+import { setupAPIClient } from '../services/api';
+import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
-  useEffect(() => {
-    // api.get('me').then(response => console.log(response));
-  }, [])
 
   return (
     <h1>Dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+  const response = await apiClient.get('me');
+
+  return {
+    props: {},
+  }
+})
